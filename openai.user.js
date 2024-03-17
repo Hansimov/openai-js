b46756d90509807f39d818635de314da81fcb236
Hansimov <591172499@qq.com>
2024-03-17 23:56:25 +0800

    :gem: [Feature] Enable chat completions post request

--- openai.user.js
+++ openai.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OpenAI.js
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.1
 // @description  Tampermonkey user script for chatting with LLM endpoints in OpenAI format
 // @author       Hansimov
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=0.1
@@ -30,10 +30,54 @@ function get_available_models({ endpoint } = {}) {
     });
 }
 
+function chat_completions({
+    messages,
+    endpoint = LLM_ENDPOINT,
+    model = "mixtral-8x7b",
+    max_tokens = -1,
+    temperature = 0.5,
+    top_p = 0.95,
+    stream = false,
+} = {}) {
+    return new Promise((resolve, reject) => {
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: endpoint + "/v1/chat/completions",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            data: JSON.stringify({
+                model: model,
+                messages: messages,
+                max_tokens: max_tokens,
+                temperature: temperature,
+                top_p: top_p,
+                stream: stream,
+            }),
+            onload: function (response) {
+                let data = JSON.parse(response.responseText);
+                let content = data.choices[0].message.content;
+                resolve(content);
+            },
+            onerror: function (error) {
+                reject(error);
+            },
+        });
+    });
+}
+
 (function () {
     "use strict";
     console.log("OpenAI.user.js Loaded.");
     get_available_models({ endpoint: LLM_ENDPOINT }).then((models) => {
         console.log("models:", models);
+    });
+    let prompt = "Hello, who are you?";
+    console.log("prompt:", prompt);
+    chat_completions({
+        messages: [{ role: "user", content: prompt }],
+        model: "mixtral-8x7b",
+    }).then((content) => {
+        console.log("content:", content);
     });
 })();
